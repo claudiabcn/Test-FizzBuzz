a940de718694ce4be424ee89b256b66b6401be16
Claudia <claudia_4bcn@hotmail.com>
2025-11-03 12:00:53 +0100

    Adding error handler in processFizzBuzz function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,21 @@
 import { calculateFizzBuzz } from "./fizzBuzz.js";
+
 const history = [];
+
 function processFizzBuzz() {
-  let number = parseInt(buzzNumber.value);
-  let response = calculateFizzBuzz(number);
-  if (response.status === "error") {
-    result.textContent = response.message;
-  } else {
-    result.textContent = response.data.result;
-    history.push(response.data);
+  try {
+    let number = parseInt(buzzNumber.value);
+    let response = calculateFizzBuzz(number);
+    if (response.status === "error") {
+      result.textContent = response.message;
+    } else {
+      result.textContent = response.data.result;
+      history.push(response.data);
+    }
+  } catch (error) {
+    result.textContent = "Error inesperado: " + error.message;
+    console.error("Error completo:", error);
   }
 }
+
 button.addEventListener("click", processFizzBuzz);
